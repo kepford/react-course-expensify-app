ba566b4d1edb2bc0adcb98d29da7355cb0c29dc1
kepford <bob@bobkepford.com>
2017-12-05 20:37:19 -0800

    Dropdowns for pick sortby

--- src/app.js
+++ src/app.js
@@ -11,13 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: 21000 }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 2000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: 3000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 1000 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -5,7 +5,7 @@ import { removeExpense } from '../actions/expenses';
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
-    <p>{amount} - {createdAt}</p>
+    <p>Amount ${amount} - Created at: {createdAt}</p>
     <button onClick={() => {
       dispatch(removeExpense({ id }));
     }}>Remove</button>
