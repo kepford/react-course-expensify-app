8ce399a09076ef8d7e494996c1f33960fb6a3d7e
kepford <bob@bobkepford.com>
2017-12-05 19:28:42 -0800

    Connecting app to react redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
@@ -12,11 +13,20 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: -1000 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: -21000 }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
-console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
