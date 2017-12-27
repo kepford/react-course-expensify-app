238f78317c066c96964e5b72998919de5db6d57e
kepford <bob@bobkepford.com>
2017-12-26 18:55:36 -0800

    Promise chaining

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
 import './firebase/firebase';
+import './playground/promises';
 
 const store = configureStore();
 
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,10 +1,10 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve({
-    //   name: 'Bob Kepford',
-    //   age: 39
-    // });
-    reject('Something went wrong');
+    resolve({
+      name: 'Bob Kepford',
+      age: 39
+    });
+    // reject('Something went wrong');
   }, 5000);
 });
 
@@ -12,6 +12,10 @@ console.log('before');
 
 promise.then((data) => {
   console.log('1', data);
+
+  return 'Some data';
+}).then((str) => {
+  console.log('does this run?', str);
 }).catch((error) => {
   console.log('error: ', error);
 });
