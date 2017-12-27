d1a9ec7872f8cc8795e003cb0405a6bff7ba6f71
kepford <bob@bobkepford.com>
2017-12-26 19:23:52 -0800

    Promises returning promises
    This helps us avoid nested callbacks. We can set up promises that return
    promises.

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -13,7 +13,13 @@ console.log('before');
 promise.then((data) => {
   console.log('1', data);
 
-  return 'Some data';
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+      // reject('Something went wrong');
+    }, 5000);
+  });
+
 }).then((str) => {
   console.log('does this run?', str);
 }).catch((error) => {
