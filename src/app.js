c987520807a1d5db455b4eeea84ba78d3c4ec415
kepford <bob@bobkepford.com>
2017-12-26 19:09:12 -0800

    Set up async tests and mock redux store
    * Adds the redux mock store lib
    * Fixes all tests

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
 import './firebase/firebase';
-import './playground/promises';
 
 const store = configureStore();
 
