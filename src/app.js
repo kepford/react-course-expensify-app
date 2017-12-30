799fa5b1e9118d479d84b543f553d3d57e7652d4
kepford <bob@bobkepford.com>
2017-12-30 11:23:27 -0800

    Adds logout functionality

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
-import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import {firebase } from './firebase/firebase';
 
 const store = configureStore();
